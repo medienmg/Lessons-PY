65c16c718c1e15c6224de1844a46fb153871eb6a
Michael Gorliak <medienmg@gmail.com>
2021-01-22 18:20:57 +0100

    updated to english

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ try:
     a = int(input("enter num1 »"))
     b = int(input("enter num2: »"))
     sum = a + b
-    print("результат: " + str(sum))
+    print("result: " + str(sum))
     input("")
     quit()
 
 except:
-    print("Ошибка.")
+    print("Error...")
     input("")
     quit()
